d63998ce4ef775ee746a8bf4151e90f3d4cb2913
swashbuck1r <swashburn@cloudbees.com>
2021-09-07 10:29:37 -0400

    fix secrets service accounts

--- helm/external-secrets/prepare.sh
+++ helm/external-secrets/prepare.sh
@@ -3,5 +3,5 @@
 helm repo add external-secrets https://external-secrets.github.io/kubernetes-external-secrets/
 helm pull external-secrets/kubernetes-external-secrets --version 1.3.0 --untar
 
-helm template external-secrets external-secrets/kubernetes-external-secrets --namespace secret-infra --include-crds --output-dir ../../manifests 
+helm template external-secrets external-secrets/kubernetes-external-secrets --namespace secret-infra --include-crds --output-dir ../../manifests -f values.yaml
 
