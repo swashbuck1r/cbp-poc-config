fb93313dab0461b63dedd6d94516461504786599
swashbuck1r <swashburn@cloudbees.com>
2021-09-07 13:27:35 -0400

    Add namespaces to cloudbees-flow templates

--- helm/cloudbees-cd/prepare.sh
+++ helm/cloudbees-cd/prepare.sh
@@ -2,7 +2,7 @@
 
 helm repo add cloudbees https://charts.cloudbees.com/public/cloudbees
 helm repo update
-helm pull cloudbees/cloudbees-flow --untar
+helm pull cloudbees/cloudbees-flow --version 2.10.2 --untar
 
 helm -n cloudbees-cd template cloudbees-cd ./cloudbees-flow --release-name --output-dir ../../manifests -f cloudbees-cd-demo.yaml -f values.yaml
 
