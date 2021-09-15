142da1ed1f1451b4f7b4b6441ba73b6346a6ee78
swashbuck1r <swashburn@cloudbees.com>
2021-09-15 10:07:58 -0400

    Add customized CloudBees CD agent to the cluster

--- helm/cloudbees-cd/prepare.sh
+++ helm/cloudbees-cd/prepare.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-helm repo add cloudbees https://charts.cloudbees.com/public/cloudbees
-helm repo update
-helm pull cloudbees/cloudbees-flow --version 2.10.2 --untar
+# helm repo add cloudbees https://charts.cloudbees.com/public/cloudbees
+# helm repo update
+# helm pull cloudbees/cloudbees-flow --version 2.10.2 --untar
 
+#generate the cloudbees-cd yaml
 helm -n cloudbees-cd template cloudbees-cd ./cloudbees-flow --release-name --output-dir ../../manifests -f cloudbees-cd-demo.yaml -f values.yaml
 
+#generate the cloudbees-cd-agent yaml
+helm -n cloudbees-agents template cloudbees-cd-agent ./cloudbees-flow/charts/cloudbees-flow-agent --release-name --output-dir ../../manifests -f agent-values.yaml
